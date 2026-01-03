cabcfc9bf9895424ffabdaafe786fbf7566748df
xiaoyangchengEd0424 <xiaoyang.cheng@gmail.com>
2026-01-02 16:26:08 -0800

    add my first python file to the repo

---
+++ python_practice.py
@@ -0,0 +1,5 @@
+# I am learning to use GIT!
+from pcinput import getInteger
+num = getInteger( "Please enter a number: " )
+print( "3 divided by {} is {}".format( num , 3/num ) )
+print( "Goodbye!" )
